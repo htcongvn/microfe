d9218ea8392af11714eeb237e09d34951992b6cd
htcongvn <40765787+htcongvn@users.noreply.github.com>
2021-03-31 15:10:43 +0700

    Randomize css style generated production classname

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+// StylesProvider is react component which is used to customize all the css-in-js generation stuff
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// webpack will generate the css styles with prefix ma in production! for all marketing app objects
+// instead of the default generated name like jss1, jss2 so that void the css style collision
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path='/pricing' component={Pricing} />
