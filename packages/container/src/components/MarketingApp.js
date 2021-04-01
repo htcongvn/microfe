e4441ebcc9b84e74e7f3109d638ef5f0ae593753
htcongvn <40765787+htcongvn@users.noreply.github.com>
2021-04-01 15:59:13 +0700

    Fixed history and browser navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,26 @@
 import { mount } from 'marketing/MarketingApp';
-
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null); // ref to the html currently displayed on the browser
+  const history = useHistory(); // history is the object copied from the browser history
 
   useEffect(() => {
-    mount(ref.current); // mount the element which is the current html on the browser (instance of MarketingApp)
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location; // current path in the url on the browser
+
+        if (pathname !== nextPathname) {
+          // upate the browser history (url on the browser) with the current visiting path
+          history.push(nextPathname);
+        }
+      },
+    }); // mount the element which is the current html on the browser (instance of MarketingApp)
+
+    // navigation down to marketing app (child) from container app (parent), i.e. when click on App link
+    history.listen(onParentNavigate);
+  }, []); // run useEffect only once when marketing app is first rendered to the screen
 
   return <div ref={ref} />; // render an instance of the ref (instance of MarketingApp) to that div
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,11 +2,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-// mount () to start up marketing app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+// mount () to start up marketing app & create a callback onNavigation()
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // create memory history navigation object. use defaultHistory of there has one, otherwise use memory history
+  // const history = defaultHistory || createMemoryHistory(); // bug!
+  const history = defaultHistory || createBrowserHistory();
+
+  // register to onNavigate callback which is defined in MarketingApp
+  // when link is clicked in marketing app, the history will be updated and the callback function will be called
+  //  and update it back to browser navigation in container app
+  if (onNavigate) {
+    // navigation up to container app (parent) from marketing app (child)
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // in development & in isolation, call mount() immediately
@@ -14,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
